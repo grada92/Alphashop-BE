5bb26a32d85763591ef6c4c00b0484e6024961f3
grada92 <danielegradassai@gmail.com>
2023-09-05 15:17:02 +0200

    modifica entity Barcode

--- src/main/java/io/danielegradassai/entity/Barcode.java
+++ src/main/java/io/danielegradassai/entity/Barcode.java
@@ -1,11 +1,15 @@
 package io.danielegradassai.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.io.Serializable;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,5 +17,16 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "BARCODE")
-public class Barcode {
+public class Barcode implements Serializable {
+
+    private static final long serialVersionUID = 1853763261962860635L;
+
+    @Id
+    @Column(name = "BARCODE")
+    private String barcode;
+
+    @Id
+    @Column(name = "IDTIPOART")
+    private String idTipoArt;
+
 }
